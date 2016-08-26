43baada0d14cad034491dfbb43765d253a6f7f85
Jeremy Fryd <fryd21@gmail.com>
2016-08-26 09:55:08 -0700

    filled html and javascript input reading begun

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+
+
+var toppings = [];
+
+
+
+
+
 $(document).ready(function() {
 
+  $("#order").submit(function(event){
+    event.preventDefault();
+    // debugger
+
+    var size = $("#size").val();
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      toppings.push($(this).val());
+    });
+    console.log(size);
+    console.log(toppings);
+  });
+
+  $("button#gratzie").click(function() {
+    // location.reload();
+  });
 });
