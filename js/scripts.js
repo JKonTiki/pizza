bf0e1e57b5920c85875c69548ff02cade9e255ea
Jeremy Fryd <fryd21@gmail.com>
2016-08-26 10:31:16 -0700

    javascript and html written with programming fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,47 @@
-
-
 var toppings = [];
+var standard = 0;
+var sizeMultiplier = 0;
+var toppingCount = 0;
+var priceNum = 0;
 
+function Pizza() {
+  this.size = 0;
+  this.toppings = [];
+}
 
-
-
+Pizza.prototype.configurePrice = function() {
+debugger
+  standard = 2;
+  if (this.size === "slice") {
+    sizeMultiplier = 1;
+  } else if (this.size === "small") {
+    sizeMultiplier = 3;
+  } else if (this.size === "medium") {
+    sizeMultiplier = 4;
+  } else if (this.size === "large") {
+    sizeMultiplier = 6;
+  }
+  toppingCount = (this.toppings).length;
+  priceNum = (standard * sizeMultiplier) + (.5 * toppingCount);
+  return "$" + priceNum.toFixed(2);
+};
 
 $(document).ready(function() {
 
   $("#order").submit(function(event){
     event.preventDefault();
-    // debugger
-
-    var size = $("#size").val();
+    var order1 = new Pizza;
+    order1.size = $("#size").val();
     $("input:checkbox[name=toppings]:checked").each(function(){
-      toppings.push($(this).val());
+      order1.toppings.push($(this).val());
     });
-    console.log(size);
-    console.log(toppings);
+    var finalPrice = order1.configurePrice();
+    $(".cost").text(finalPrice);
+    $("#response").show();
   });
 
   $("button#gratzie").click(function() {
-    // location.reload();
+    location.reload();
   });
+
 });
--- readme.md
+++ readme.md
@@ -1,15 +1,25 @@
-# Starter Pack
+# Pizza Orderer
 
-This is a starter pack for Epicodus Intro to Programming Projects.
+####With this nifty app, users can simulate the experience of ordering a pizza in a virtual yet authentic Italian restaurant. No need to suppress your temptation - try it out for yourself here: https://jkontiki.github.io/pizza/.
 
-## Installation
+#### By Jeremy Fryd
 
-Clone the repository and use it to start your own Epicodus daily projects. 
+##Description
+
+Users begin the experience by loading the page. They then enter into a highly immersive experience in which they interact with the AI through usage of a series of buttons and forms.
+
+## Specs
+
+* It can read user inputs and calculate them into a price returned to the user*
+  * Example Input: pepperoni and cheese on a small pizza*
+  * Example Output: "$7.00"
+
+## Technologies Used
+
+This web page was created using HTML, CSS, and Javascript.
 
 ## Credits
 
-Marta Sánchez
-
 This software is licensed under MIT license.
 
-Copyright (c) 2016 Marta Sanchez
+Copyright (c) 2016 Jeremy Fryd.
